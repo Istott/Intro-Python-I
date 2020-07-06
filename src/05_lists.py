0fd58cb7c1c1c06404c0bb3eb966d0dd52afbec6
isaac stott <isaac.cloyd@gmail.com>
2020-07-06 14:13:42 -0600

    up and running, rocking and rolling

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,19 +8,28 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+for el in x:
+    print(el)
+
+# for i, el in enumerate(x):
+#     print(i, el)
+
+for x in range(2, 4):
+    print(x)
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+# print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -17,7 +17,7 @@ print (y)
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [n*n for n in range(10)]
 
 print(y)
 
